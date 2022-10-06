8fcd41f80f33d89138da1bafe6eb51fb90e61e0d
Jakob Troidl <jakob.troidl@googlemail.com>
2022-10-06 11:29:37 -0400

    adding comments

--- app.py
+++ app.py
@@ -7,46 +7,46 @@ DATABASE = os.path.join(PROJECT_ROOT, 'todo.db')
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////" + DATABASE
-db = SQLAlchemy(app)
+db = SQLAlchemy(app)  # generate database object
 
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80))
-    complete = db.Column(db.Boolean)
+class Todo(db.Model): # class for a to_do item
+    id = db.Column(db.Integer, primary_key=True)  # id of the item
+    title = db.Column(db.String(80))  # name of todo
+    complete = db.Column(db.Boolean)  # boolean if done or not
 
 
 @app.route("/")
-def index():
+def index():  # render all to_do item in the database when the page is loaded
     todo_list = Todo.query.all()
-    return render_template("index.html", todo_list=todo_list)
+    return render_template("index.html", todo_list=todo_list)  # render index.html with the list of to_do items
 
 
-@app.route("/add", methods=["POST"])
+@app.route("/add", methods=["POST"])  # add new to_do item, POST request to databse
 def add():
-    title = request.form.get("title")
-    new_todo = Todo(title=title, complete=False)
-    db.session.add(new_todo)
-    db.session.commit()
-    return redirect(url_for("index"))
+    title = request.form.get("title")  # get to_do title from request
+    new_todo = Todo(title=title, complete=False)  # create new to_do item
+    db.session.add(new_todo)  # add to_do item to database
+    db.session.commit()  # commit changes to database
+    return redirect(url_for("index"))  # redirect to index page
 
 
 @app.route("/complete/<string:todo_id>")
-def complete(todo_id):
-    todo = Todo.query.filter_by(id=todo_id).first()
-    todo.complete = not todo.complete
-    db.session.commit()
+def complete(todo_id):  # mark to_do item as complete
+    todo = Todo.query.filter_by(id=todo_id).first()  # get to_do item from database
+    todo.complete = not todo.complete  # change complete status
+    db.session.commit()  # commit changes to database
     return redirect(url_for("index"))
 
 
 @app.route("/delete/<string:todo_id>")
-def delete(todo_id):
-    todo = Todo.query.filter_by(id=todo_id).first()
-    db.session.delete(todo)
-    db.session.commit()
+def delete(todo_id):  # method to delete to_do item
+    todo = Todo.query.filter_by(id=todo_id).first()  # find to_do item in database
+    db.session.delete(todo)  # delete to_do item from database
+    db.session.commit()  # commit changes to database
     return redirect(url_for("index"))
 
 
-if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True)
+if __name__ == "__main__":  # entry point for the application
+    db.create_all()  # create database if it doesn't exist
+    app.run(debug=True)  # run the flask app
